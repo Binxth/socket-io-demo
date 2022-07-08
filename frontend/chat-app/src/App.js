c8b6f8d4dc4b709af9e6cdbd5859c12b98b04e50
Binxth <33088210+Binxth@users.noreply.github.com>
2022-07-08 14:35:14 +0530

    Setting up frontend - react app

---
+++ frontend/chat-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import {useState, useEffect} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Socket.io Chat</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
